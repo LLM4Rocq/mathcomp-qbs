42422e1924ff382542eef652844a229d9d89b9a4
Guillaume Baudart <guillaume.baudart@inria.fr>
2026-04-02 12:54:54 +0000

    Add .gitignore, remove cached files from tracking

--- rocq_mcp_cache_105798_.v
+++
@@ -1,4 +0,0 @@
-From mathcomp Require Import all_boot all_algebra reals.
-From mathcomp.algebra_tactics Require Import ring.
-Import GRing.Theory Num.Def Num.Theory.
-Open Scope ring_scope.
--- rocq_mcp_cache_163171_.v
+++
@@ -1,7 +0,0 @@
-From mathcomp Require Import all_boot all_algebra ring.
-From mathcomp.reals Require Import reals.
-Import GRing.Theory Num.Theory.
-Local Open Scope ring_scope.
-Section T.
-Variable R : realType.
-Variables a b s s' : R.
